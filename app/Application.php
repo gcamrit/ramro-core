8fd8f348b10be90735e2af91bd2ad07ed0456077
Amrit G.C <music.demand01@gmail.com>
2017-04-13 19:27:35 +0545

    Bundle packages together - [x] Add app directory for Connecting packages - [x] Add app directory on psr-4 autoload on Ramro namespace - [x] Create a Application class for bootstrapping

---
+++ app/Application.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ramro;
+
+use League\Container\ContainerAwareInterface;
+use League\Container\ContainerInterface;
+
+class Application implements ContainerAwareInterface
+{
+
+    /**
+     * Set a container
+     *
+     * @param \League\Container\ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container)
+    {
+        // TODO: Implement setContainer() method.
+    }
+
+    /**
+     * Get the container
+     *
+     * @return \League\Container\ContainerInterface
+     */
+    public function getContainer()
+    {
+        // TODO: Implement getContainer() method.
+    }
+}
